2520dfe85cd5993351f87c4443830149cf9f0c76
Bartłomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2021-10-27 21:59:22 +0200

    let's create some widgets!

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ import msvcrt
 import sys
 
 
+class ConsoleForm:
+    def __init__(self, x: int, y: int, width: int, height: int):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+
 class Console:
     def __init__(self, debug=True):
         self.brush = Brush(self)
