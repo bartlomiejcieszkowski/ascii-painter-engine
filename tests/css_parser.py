cbb17e7e6b200f1cd9d3ce3962892a487d3b6063
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2022-09-15 13:36:10 +0200

    css_parser test

---
+++ tests/css_parser.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import ascii_painter_engine as ape
+from ascii_painter_engine.theme import CssParser
+
+
+def test(handle_sigint=True, demo_time_s=None):
+    app = ape.App(log=ape.log.log)
+
+    working_directory = 'tests'
+    files = [
+        'css_parser/main.css'
+    ]
+
+    for file in files:
+        selectors = CssParser.parse(working_directory + '/' + file, None)
+
+    app.handle_sigint = handle_sigint
+    app.demo_mode(demo_time_s)
+
+    app.run()
