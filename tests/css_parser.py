a142ad9e06143d92bf44ace7435bdc42921a81a3
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2022-09-15 13:40:08 +0200

    css_parser - no app for this test

--- tests/css_parser.py
+++ tests/css_parser.py
@@ -4,7 +4,7 @@ from ascii_painter_engine.theme import CssParser
 
 
 def test(handle_sigint=True, demo_time_s=None):
-    app = ape.App(log=ape.log.log)
+    #app = ape.App(log=ape.log.log)
 
     working_directory = 'tests'
     files = [
@@ -14,7 +14,7 @@ def test(handle_sigint=True, demo_time_s=None):
     for file in files:
         selectors = CssParser.parse(working_directory + '/' + file, None)
 
-    app.handle_sigint = handle_sigint
-    app.demo_mode(demo_time_s)
+    #app.handle_sigint = handle_sigint
+    #app.demo_mode(demo_time_s)
 
-    app.run()
+    #app.run()
